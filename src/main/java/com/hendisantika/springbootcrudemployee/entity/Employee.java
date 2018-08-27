954634512a64ba48811d9ae07f6ff9d1383764c4
Hendi Santika <hendisantika@gmail.com>
2018-08-28 05:56:02 +0700

    #3 - Add Employee Entity

---
+++ src/main/java/com/hendisantika/springbootcrudemployee/entity/Employee.java
@@ -0,0 +1,44 @@
+package com.hendisantika.springbootcrudemployee.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-crud-employee
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 28/08/18
+ * Time: 05.55
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Data
+public class Employee {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    @Column(nullable = false)
+    @NotNull
+    @NotEmpty
+    @Size(min = 3, max = 150)
+    private String nama;
+
+    @Column(nullable = false, unique = true)
+    @Email
+    @NotEmpty
+    private String email;
+
+}
