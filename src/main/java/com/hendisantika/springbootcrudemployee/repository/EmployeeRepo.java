96d549a9488be162a31a1d7a489eac6b44cb1cc3
Hendi Santika <hendisantika@gmail.com>
2018-08-28 05:57:17 +0700

    #4 - Add Employee Repository

---
+++ src/main/java/com/hendisantika/springbootcrudemployee/repository/EmployeeRepo.java
@@ -0,0 +1,20 @@
+package com.hendisantika.springbootcrudemployee.repository;
+
+import com.hendisantika.springbootcrudemployee.entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-crud-employee
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 28/08/18
+ * Time: 05.56
+ * To change this template use File | Settings | File Templates.
+ */
+public interface EmployeeRepo extends PagingAndSortingRepository<Employee, String> {
+    Page<Employee> findByNamaContainingIgnoreCase(String nama, Pageable pageable);
+}
