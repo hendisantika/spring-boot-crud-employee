db581d80dd610d336421ba843204036ad7cecd88
Hendi Santika <hendisantika@gmail.com>
2018-08-28 05:53:19 +0700

    #1 - first commit

---
+++ src/main/java/com/hendisantika/springbootcrudemployee/SpringBootCrudEmployeeApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootcrudemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudEmployeeApplication.class, args);
+    }
+}
