170d40076697520d887a5ddc76b9a46fedbfbde4
Hendi Santika <hendisantika@gmail.com>
2018-08-29 06:32:57 +0700

    #23 - Bug Fix V3__Insert_Data_User.sql migration script

--- src/main/resources/db/migration/V3__Insert_Data_User.sql
+++ src/main/resources/db/migration/V3__Insert_Data_User.sql
@@ -1,4 +1,7 @@
 INSERT INTO s_users VALUES
 ('u003','naruto','$2a$10$T0lWqk.pk2guoCxhCqPS9.HHxX6TSIL2559Gk7qNIF0hODKtTXda.',true),
-('u004','kakashi','$2a$10$T0lWqk.pk2guoCxhCqPS9.HHxX6TSIL2559Gk7qNIF0hODKtTXda.',true);
-('u004','sasuke','$2a$10$T0lWqk.pk2guoCxhCqPS9.HHxX6TSIL2559Gk7qNIF0hODKtTXda.',true);
+('u004','kakashi','$2a$10$T0lWqk.pk2guoCxhCqPS9.HHxX6TSIL2559Gk7qNIF0hODKtTXda.',true),
+('u005','sasuke','$2a$10$T0lWqk.pk2guoCxhCqPS9.HHxX6TSIL2559Gk7qNIF0hODKtTXda.',true);
+
+INSERT INTO s_user_role VALUES
+('u003','stf'),('u004','stf'),('u005','stf');
