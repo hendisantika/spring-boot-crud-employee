55e572382ee593f2518d0a8ad5ab84e9729901a4
Hendi Santika <hendisantika@gmail.com>
2018-08-29 06:13:55 +0700

    #22 - Add V3__Insert_Data_User.sql migration script

---
+++ src/main/resources/db/migration/V3__Insert_Data_User.sql
@@ -0,0 +1,4 @@
+INSERT INTO s_users VALUES
+('u003','naruto','$2a$10$T0lWqk.pk2guoCxhCqPS9.HHxX6TSIL2559Gk7qNIF0hODKtTXda.',true),
+('u004','kakashi','$2a$10$T0lWqk.pk2guoCxhCqPS9.HHxX6TSIL2559Gk7qNIF0hODKtTXda.',true);
+('u004','sasuke','$2a$10$T0lWqk.pk2guoCxhCqPS9.HHxX6TSIL2559Gk7qNIF0hODKtTXda.',true);
